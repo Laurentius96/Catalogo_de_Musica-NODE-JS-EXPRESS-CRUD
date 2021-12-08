36a947ff163546904199e81ce2fe891caa98196d
Lorenzo C. Bianchi <bianchi92@hotmail.com>
2021-12-07 21:18:36 -0300

    Add o put e o post no arquivo albuns.controller e o post na albuns.route

--- BackEnd/controllers/albuns.controller.js
+++ BackEnd/controllers/albuns.controller.js
@@ -21,8 +21,37 @@ const getAlbunsById = (req, res) => {
   res.send(album);
 };
 
+// 24°) Irá cadastrar um novo album de acordo com o objeto vindo do Font-End...
+const postAlbum = (req, res) => {
+  // É pego o objeto da requisição para ser cadastrado...
+  const album = req.body;
+  // É enviado o album que recebeu via corpo de requisição para o serviço adicionado na lista...
+  const newAlbum = vagasService.addAlbum(album);
+  res.send({
+    message: `O álbum ${newAlbum.name} do(a) artista/banda ${newAlbum.artista} foi adicionada ao catálogo!`,
+  });
+};
+
+// 26°) Vai receber um objeto(body) e um ID(param) para poder atualizar a vaga com o objeto de acordo com seu ID...
+const putAlbum = (req, res) => {
+  // Pega o parametro via requisição..
+  const idParam = req.params.id;
+  // Pega o objeto recebido via requisição...
+  const albumEdit = req.body;
+  const edicao = albunsService.putAlbum(idParam, albumEdit);
+  if (edicao) {
+    res.send({ message: `O álbum foi editado com sucesso!` });
+  } else {
+    res
+      .status(404)
+      .send({ message: `Não foi encontrado o álbum com o ID digitado!` });
+  }
+};
+
 // 14°) Exportan as funções para serem usadas nas rotas...
 module.exports = {
   getAlbuns,
   getAlbunsById,
+  postAlbum,
+  putAlbum,
 };
